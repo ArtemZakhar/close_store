85776d6c2df3ee9654c1219b8b2862c7d3bb1bdf
ArtemZakharchukWWG <z.artem@wwg.it>
2024-12-18 10:45:41 +0200

    try this one

--- src/app/api/constants/apiCalls.ts
+++ src/app/api/constants/apiCalls.ts
@@ -1,16 +1,16 @@
 export const apiCalls = {
-  categories: 'api/goods/categories',
+  categories: '/api/goods/categories',
   categoriesUpdate(id: string) {
-    return `api/goods/categories/${id}`;
+    return `/api/goods/categories/${id}`;
   },
-  cities: 'api/cities',
-  countries: 'api/countries',
-  forgotPassword: 'api/auth/forgot-password',
-  finishRegistration: 'api/users/finish-registration',
-  firms: 'api/goods/firms',
-  login: 'api/auth/login',
-  logout: 'api/auth/logout',
-  newPassword: 'api/auth/new-password',
-  sellers: 'api/goods/sellers',
-  users: 'api/users',
+  cities: '/api/cities',
+  countries: '/api/countries',
+  forgotPassword: '/api/auth/forgot-password',
+  finishRegistration: '/api/users/finish-registration',
+  firms: '/api/goods/firms',
+  login: '/api/auth/login',
+  logout: '/api/auth/logout',
+  newPassword: '/api/auth/new-password',
+  sellers: '/api/goods/sellers',
+  users: '/api/users',
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,20 +4,21 @@ import { decrypt } from './helpers/auth';
 
 export default async function middleware(req: NextRequest) {
   const session = req.cookies.get('session')?.value || null;
-  const { origin } = req.nextUrl;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || req.nextUrl.origin;
+  console.log(origin);
 
   if (!session) {
-    return NextResponse.redirect(`${origin}/login`);
+    return NextResponse.redirect(new URL('/login', baseUrl));
   }
 
   const parsed = await decrypt(session);
 
   if (!parsed) {
-    return NextResponse.redirect(`${origin}/login`);
+    return NextResponse.redirect(new URL('/login', baseUrl));
   }
 
   if (parsed && parsed.exp && parsed.iat && parsed.exp <= parsed.iat) {
-    return NextResponse.redirect(`${origin}/login`);
+    return NextResponse.redirect(new URL('/login', baseUrl));
   }
 
   return NextResponse.next();
