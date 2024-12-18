5f9e98c28a8f62c870ee7e7194b28c4b7b1ce68a
ArtemZakharchukWWG <z.artem@wwg.it>
2024-12-18 10:39:54 +0200

    add -

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,17 @@ export default async function middleware(req: NextRequest) {
   const { origin } = req.nextUrl;
 
   if (!session) {
-    return NextResponse.redirect(`${origin}login`);
+    return NextResponse.redirect(`${origin}/login`);
   }
 
   const parsed = await decrypt(session);
 
   if (!parsed) {
-    return NextResponse.redirect(`${origin}login`);
+    return NextResponse.redirect(`${origin}/login`);
   }
 
   if (parsed && parsed.exp && parsed.iat && parsed.exp <= parsed.iat) {
-    return NextResponse.redirect(`${origin}login`);
+    return NextResponse.redirect(`${origin}/login`);
   }
 
   return NextResponse.next();
